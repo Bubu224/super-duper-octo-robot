9ccc1bd37863a12431e58ec785f4bd356a54739a
Busisiwe <hlophebusisiwe866@gmail.com>
2024-05-15 13:31:52 +0200

    all done

--- leap_year/is_leap_year.py
+++ leap_year/is_leap_year.py
@@ -1,2 +1,11 @@
 def is_leap(year):
-    pass
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
